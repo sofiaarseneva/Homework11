b480adc14866ec43c9a9c83a8da19aba0ebe4593
Sofia <151773@herzen.spb.ru>
2024-10-10 11:10:18 +0300

    Добавьте файлы проекта.

---
+++ Task1/Linear.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal struct Linear
+    {
+        //структура для решения линейного уравнения kx+b=0
+        double k;
+        double b;
+
+        public Linear(double k, double b)
+        {
+            this.k = k;
+            this.b = b;
+        }
+
+        public string Root()
+        {
+            //в таком случае получаем уравнение 0 * х + 0 = 0, т.е. 0 * х = 0
+            if (k == 0 && b == 0)
+                return "Уравнение имеет бесконечно много корней!";
+            //в таком случае получаем уравнение 0 * х + b = 0, т.е. 0 * x = - b, что не имеет решения 
+            if (k == 0 && b != 0)
+                return "Уравнение не имеет корней!";
+            //выразим х через коэффициенты kx = -b, x = - b / k
+            double x = - (b / k);
+
+            return $"Уравнение имеет единственный корень x = {x}!";
+        }
+    }
+}
---
+++ Task1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Решение линейного уравнения kx + b = 0!\n\n");
+            //на случай возникновения exception, например, ввода строки вместо числа...
+            try
+            {
+                Console.WriteLine("Введите коэффициент k:");
+                double k = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Введите коэффициент b:");
+                double b = Convert.ToDouble(Console.ReadLine());
+
+                Linear linear = new Linear(k, b);
+                Console.WriteLine(linear.Root());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ошибка! " + ex.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
